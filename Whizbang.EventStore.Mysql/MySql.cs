fd7c07fbe125e683a7a6f6c85428f41f1ddc42fc
mscorlib <mscorlib@live.com>
2020-06-10 17:28:28 +0800

    implement event-store with mysql

---
+++ Whizbang.EventStore.Mysql/MySql.cs
@@ -0,0 +1,12 @@
+﻿using Whizbang.Core.Data.FluentSqlClient;
+
+namespace Whizbang.EventStore.MySql
+{
+    public static class MySql
+    {
+        public static IExecutor With(string connectionString)
+        {
+            return new MySqlExecutor(connectionString);
+        }
+    }
+}
---
+++ Whizbang.EventStore.Mysql/MySqlExecutor.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+using Whizbang.Core.Data.FluentSqlClient;
+
+namespace Whizbang.EventStore.MySql
+{
+    public class MySqlExecutor : Executor
+    {
+        public MySqlExecutor(string connectionString)
+            : base(
+                () => new MySqlConnection(connectionString),
+                () => new MySqlCommand(),
+                (cmd, paramName, value) => ((MySqlCommand) cmd).Parameters.AddWithValue(paramName, value))
+        {
+        }
+    }
+}
